a5f2af4638c01e576aeb7b357863e7160791f5ab
shooter <shooter.il@yandex.ua>
2016-03-04 00:07:11 +0200

    not work as pyl

--- play.py
+++ play.py
@@ -5,18 +5,18 @@ import rk4
 from math import atan, cos
 
 
-h = 0.001
+h = 0.01
 t_max = 100
 t = np.arange(0, t_max+h, h)
 
-b = 0.01
-w0 = 0.5
+b = 3
+w0 = 80
 
-x0 = 5
-y0 = 0
+x0 = 1
+y0 = 1
 
 f0 =0
-w = 0.2
+w = 0
 
 
 
@@ -41,29 +41,33 @@ x_real = A0*np.exp(-b*t)*np.cos(t*np.sqrt(w0*w0-b*b) + phi)
 
 for i in range(1, len(t)):
     y[i] = rk4.rk4(x[i-1], y[i-1], f_in, h)
+    #y[i] = rk4.rk4(x[i-1], y[i-1], lambda x, y: f_out(t[i-1], x, y), h)
     x[i] = rk4.rk4(y[i-1], x[i-1], f, h)
 
 
 
-a = plt.subplot(311)
-plt.plot(t, x_real)
+plt.subplot(311)
+plt.plot(t, x)
 plt.xlabel(r'$t$')
-plt.ylabel(r'$x_real$')
+plt.ylabel(r'$x$')
 plt.grid(True)
 
 plt.subplot(312)
-plt.plot(t, x)
-plt.xlabel(r'$t$')
-plt.ylabel(r'$dev(x)$')
-plt.grid(True)
-
-
-plt.subplot(313)
-plt.plot(t, y)
+plt.plot(x, y)
 plt.xlabel(r'$x$')
-plt.ylabel(r'$x$')
+plt.ylabel(r'$der(x)$')
 plt.grid(True)
-plt.legend(lob ='upper right')
+
+
+# plt.subplot(313)
+# plt.plot(t, y)
+# plt.xlabel(r'$x$')
+# plt.ylabel(r'$der(x)$')
+# plt.grid(True)
+#
+# plt.plot(x,y)
+# plt.axis('equal')
+# plt.grid(True)
 
 plt.show()
 
