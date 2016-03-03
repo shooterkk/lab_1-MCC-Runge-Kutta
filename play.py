8d6cbfb2250df213f73cd7105b7d7cf2eb8cb04d
shooter <shooter.il@yandex.ua>
2016-03-04 01:24:10 +0200

    strong changes

--- play.py
+++ play.py
@@ -5,18 +5,19 @@ import rk4
 from math import atan, cos
 
 
-h = 0.01
-t_max = 100
-t = np.arange(0, t_max+h, h)
+h = 0.0001
+t_max = 50
+t = np.arange(0, t_max, h)
+print(t[-1])
 
-b = 3
-w0 = 80
+b = 0
+w0 = 9
 
 x0 = 1
 y0 = 1
 
-f0 =0
-w = 0
+f0 =5
+w = 8
 
 
 
@@ -40,19 +41,25 @@ A0 = x0/(cos(atan(phi)))
 x_real = A0*np.exp(-b*t)*np.cos(t*np.sqrt(w0*w0-b*b) + phi)
 
 for i in range(1, len(t)):
-    y[i] = rk4.rk4(x[i-1], y[i-1], f_in, h)
-    #y[i] = rk4.rk4(x[i-1], y[i-1], lambda x, y: f_out(t[i-1], x, y), h)
+    #y[i] = rk4.rk4(x[i-1], y[i-1], f_in, h)
+    y[i] = rk4.rk4(x[i-1], y[i-1], lambda x, y: f_out(t[i-1], x, y), h)
     x[i] = rk4.rk4(y[i-1], x[i-1], f, h)
 
-
+print(np.linalg.norm(x_real - x), len(t), len(x))
 
 plt.subplot(311)
-plt.plot(t, x)
+plt.plot(t, x_real)
 plt.xlabel(r'$t$')
 plt.ylabel(r'$x$')
 plt.grid(True)
 
 plt.subplot(312)
+plt.plot(t, x)
+plt.xlabel(r'$t$')
+plt.ylabel(r'$x$')
+plt.grid(True)
+
+plt.subplot(313)
 plt.plot(x, y)
 plt.xlabel(r'$x$')
 plt.ylabel(r'$der(x)$')
--- rk4.py
+++ rk4.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def rk4(x0, y0, f, h):
     k1 = f(x0, y0)
-    k2 = f(x0 + 0.5*h, y0 + 0.5*k1)
-    k3 = f(x0 + 0.5*h, y0 + 0.5*k2)
-    k4 = f(x0 + h, y0 + k3)
+    k2 = f(x0 + 0.5*h, y0 + 0.5*h*k1)
+    k3 = f(x0 + 0.5*h, y0 + 0.5*h*k2)
+    k4 = f(x0 + h, y0 + h*k3)
     return y0 + h*(k1 + 2*k2 + 2*k3 + k4)/6
