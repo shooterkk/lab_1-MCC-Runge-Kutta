e424580c987d24ad1718dc41d7e45d906a70dc2f
shooter <shooter.il@yandex.ua>
2016-03-04 09:36:30 +0200

    add AChHa to plot

--- play.py
+++ play.py
@@ -6,18 +6,18 @@ from math import atan, cos
 
 
 h = 0.0001
-t_max = 50
+t_max = 5
 t = np.arange(0, t_max, h)
 print(t[-1])
 
-b = 0
-w0 = 9
+b = 3
+w0 = 80
 
 x0 = 1
 y0 = 1
 
-f0 =5
-w = 8
+f0 =1
+w = 5
 
 
 
@@ -38,39 +38,41 @@ y[0] = y0
 
 phi = atan(-y0/(w0*x0))
 A0 = x0/(cos(atan(phi)))
-x_real = A0*np.exp(-b*t)*np.cos(t*np.sqrt(w0*w0-b*b) + phi)
+#x_real = A0*np.exp(-b*t)*np.cos(t*np.sqrt(w0*w0-b*b) + phi)
 
 for i in range(1, len(t)):
     #y[i] = rk4.rk4(x[i-1], y[i-1], f_in, h)
     y[i] = rk4.rk4(x[i-1], y[i-1], lambda x, y: f_out(t[i-1], x, y), h)
     x[i] = rk4.rk4(y[i-1], x[i-1], f, h)
 
-print(np.linalg.norm(x_real - x), len(t), len(x))
+#print(np.linalg.norm(x_real - x), len(t), len(x))
+
+# plt.subplot(311)
+# plt.plot(t, x_real)
+# plt.xlabel(r'$t$')
+# plt.ylabel(r'$x$')
+# plt.grid(True)
+omega = np.arange(0, 50, h)
+AChH = f0/np.sqrt((w0**2-omega**2)**2+4*(b*omega)**2)
 
 plt.subplot(311)
-plt.plot(t, x_real)
-plt.xlabel(r'$t$')
-plt.ylabel(r'$x$')
-plt.grid(True)
-
-plt.subplot(312)
 plt.plot(t, x)
 plt.xlabel(r'$t$')
 plt.ylabel(r'$x$')
 plt.grid(True)
 
-plt.subplot(313)
+plt.subplot(312)
 plt.plot(x, y)
 plt.xlabel(r'$x$')
 plt.ylabel(r'$\dot{x}$')
 plt.grid(True)
 
 
-# plt.subplot(313)
-# plt.plot(t, y)
-# plt.xlabel(r'$x$')
-# plt.ylabel(r'$der(x)$')
-# plt.grid(True)
+plt.subplot(313)
+plt.plot(omega, AChH)
+plt.xlabel(r'$\omega$')
+plt.ylabel(r'$A$')
+plt.grid(True)
 #
 # plt.plot(x,y)
 # plt.axis('equal')
