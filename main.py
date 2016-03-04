d2dc6543fabb7c262193e0a9e4267af29897ee0a
Megalitum <fatvlad.95@gmail.com>
2016-03-04 10:29:16 +0200

    interface completed

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 #coding: utf8
 import sys
+import matplotlib
+
+matplotlib.use("Qt5Agg", force=True)
 
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QDialog
 from PyQt5.uic import loadUiType
 from custom_canvas import StaticCanvas
+
+
+import numpy as np
 
 app = QApplication(sys.argv)
 app.setApplicationName('Lab 1')
@@ -21,30 +27,78 @@ class MainWindow(QDialog, form_class):
         self.x_t_layout.addWidget(self.x_t)
         self.x_dx_layout.addWidget(self.x_dx)
         self.afc_layout.addWidget(self.afc)
+        self.delta = 0
+        self.w0 = 0
+        self.f0 = 0
+        self.w = 0
+        self.left_bound = 0
+        self.right_bound = 0
+        self.x0 = 0
+        self.dx0 = 0
+        self.update_coefficients()
 
     @pyqtSlot()
     def build_graph(self):
-        pass
+        try:
+            self.delta = np.float64(self.delta_input.text())
+            self.w0 = np.float64(self.w0_input.text())
+            self.f0 = np.float64(self.f0_input.text())
+            self.w = np.float64(self.w_input.text())
+        except:
+            return
+        # plot graphs here
 
     @pyqtSlot()
     def template_selected(self):
-        pass
-
-    @pyqtSlot('QString')
-    def coef_changed(self, value):
-        pass
+        sender = self.sender().objectName()
+        if sender == 'harmonic_rad':
+            self.delta = 10
+            self.w0 = 20
+            self.f0 = 0
+            self.w = 0
+            self.update_coefficients()
+        elif sender == 'dying_rad':
+            self.delta = 10
+            self.w0 = 1
+            self.f0 = 0
+            self.w = 0
+            self.update_coefficients()
+        elif sender == 'forced_rad':
+            self.delta = 5
+            self.w0 = 80
+            self.f0 = 3
+            self.w = 10
+            self.update_coefficients()
 
     @pyqtSlot(int)
-    def limit_change(self, velue):
-        pass
+    def limit_change(self, value):
+        self.right_bound = value
 
     @pyqtSlot('double')
     def step_change(self, value):
-        pass
+        self.step = value
 
     @pyqtSlot('QString')
     def initval_change(self, value):
-        pass
+        sender = self.sender().objectName()
+        try:
+            value = np.float64(value)
+        except:
+            return
+        if sender == 'x0_input':
+            self.x0 = value
+        elif sender == 'dx0_input':
+            self.dx0 = value
+
+    def update_coefficients(self):
+        self.delta_input.setText(str(self.delta))
+        self.w0_input.setText(str(self.w0))
+        self.f0_input.setText(str(self.f0))
+        self.w_input.setText(str(self.w))
+
+    @pyqtSlot('QString')
+    def coef_edited(self, value):
+        self.other_rad.setChecked(True)
 
 
 
